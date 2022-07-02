a784fa6e80bf2733d767caa59a9b956ed7597b81
Whaleless <vjblhepmz11@mail.ru>
2022-07-02 15:17:55 +0300

    mobile_version

--- script.js
+++ script.js
@@ -14,48 +14,13 @@ let advicer = function() {
         cache: 'no-cache'
     }).then(function (advice) {
         advice.json().then(function (obj) {
-            if (body.classList.contains('insultBody')) {
-                body.classList.replace('insultBody', 'normalBody');
-                main.classList.replace('insultMain', 'normalMain');
-                adviceText.classList.replace('insultAdvice', 'normalAdvice');
-                adviceID.classList.replace('insultID', 'normalID');
-            }
             adviceID.textContent = 'Advice #' + obj.slip.id;
             adviceText.textContent = obj.slip.advice;
-            console.log('ID: ' + obj.slip.id);
-            console.log(obj.slip.advice.length);
         })
     })
 }
-
-let insulter = function () {
-    let insult = fetch('https://cors-anywhere.herokuapp.com/https://evilinsult.com/generate_insult.php?lang=en&amp;type=json', {
-        method: 'GET',
-        cache: 'no-cache',
-        accept: 'application/json',
-        mode: 'cors'
-    }).then(function (insult){
-        insult.text().then(function (obj){
-            console.log(obj);
-            adviceID.textContent = 'Advice #INSULT';
-            adviceText.textContent = obj;
-            body.classList.replace('normalBody', 'insultBody');
-            main.classList.replace('normalMain', 'insultMain');
-            adviceText.classList.replace('normalAdvice', 'insultAdvice');
-            adviceID.classList.replace('normalID', 'insultID');
-        })
-    })
-}
-
-let clickNum = 0;
 
 button.addEventListener('click', function () {
-    clickNum++;
-    if (clickNum % 10 == 0) {
-        insulter();
-    }
-    else {
-        advicer()
-    }
+        advicer();
         });
 
